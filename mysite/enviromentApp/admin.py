7b6905bbe99dc89fe35234d034cbe7596512f623
Jeffrey Zhao <jeffreyzhao2003@gmail.com>
2020-11-05 23:19:43 -0500

    updated the admin page

--- mysite/enviromentApp/admin.py
+++ mysite/enviromentApp/admin.py
@@ -2,15 +2,35 @@ from django.contrib import admin
 from .models import promo, resource, email
 from django.http import HttpResponse
 import csv
+import os
 
 # Register your models here.
+
+def removeData(data):
+    try:
+        if str(data.photo) != 'static/img/logo.png':
+            os.remove(str(data.photo))
+    finally:
+        return(data.delete())
+
 class PromoAdmin(admin.ModelAdmin):
     list_display = ('title','date', 'description','link','photo')
     search_fields = ['title']
+    actions = ['delete_selected']
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            removeData(obj)
+
 
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('title','date', 'description', 'photo')
     search_fields =['title']
+    actions = ['delete_selected']
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            removeData(obj)
 
 
 def getEmails(self, request, queryset):
--- mysite/enviromentApp/models.py
+++ mysite/enviromentApp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-import uuid
+import datetime
 
 # Create your models here.
 
 class promo(models.Model):
     title = models.CharField(max_length=140, help_text='Title for the promo')
-    date = models.DateTimeField(help_text='Date for the promo. If it is year long, put the end date')
+    date = models.DateTimeField(default=datetime.datetime.now(), help_text='Date for the promo. If it is year long, put the end date')
     description = models.TextField(blank=True, help_text='Description for the resource')
     link = models.URLField(blank=True, help_text='Link to the website assoicated')
     photo = models.ImageField(default='static/img/logo.png', upload_to='static/img/promo')
@@ -16,7 +16,7 @@ class promo(models.Model):
 class resource(models.Model):
     title = models.CharField(max_length=140, help_text='Title for the promo')
     description = models.TextField(blank=True, help_text='Description for the resource')
-    date = models.DateTimeField(help_text='Date the resource was created')
+    date = models.DateTimeField(default=datetime.datetime.now(), help_text='Date the resource was created')
     photo = models.ImageField(default='static/img/logo.png', upload_to='static/img/resource')
 
     def __str__(self):
